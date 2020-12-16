4b7dc9f735b039e510996237df6d9478e8c81c82
rayyu999 <411205547@qq.com>
2020-12-16 11:14:51 +0800

    jzoffer 14-I

--- src/dp/JZOffer14_1.java
+++ src/dp/JZOffer14_1.java
@@ -3,13 +3,17 @@ package dp;
 public class JZOffer14_1 {
 
     public int cuttingRope(int n) {
-        int[] dp = new int[n + 1];
+        int[] product = new int[n + 1];
+        product[1] = 1;
+        // 从下往上计算
         for (int i = 2; i <= n; ++i) {
+            // 枚举剪绳子的位置
             for (int j = 1; j * 2 <= i; ++j) {
-                dp[i] = Math.max(dp[i], Math.max(j, dp[j]) * Math.max(i-j, dp[i-j]));
+                // 计算当前绳子长度能拆分乘积的最大值
+                product[i] = Math.max(product[i], Math.max(j, product[j]) * Math.max(i-j, product[i-j]));
             }
         }
-        return dp[n];
+        return product[n];
     }
 
 }
