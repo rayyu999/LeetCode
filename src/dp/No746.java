0e25f24d2781f7fc912935f824396dbafb028f8e
rayyu999 <411205547@qq.com>
2020-12-21 21:10:49 +0800

    lc 746

---
+++ src/dp/No746.java
@@ -0,0 +1,16 @@
+package dp;
+
+public class No746 {
+
+    public int minCostClimbingStairs(int[] cost) {
+        int len = cost.length;
+        if (len < 3) return Math.min(cost[0], cost[1]);
+        int[] dp = new int[len+1];
+        dp[0] = dp[1] = 0;
+        for (int i = 2; i <= len; ++i) {
+            dp[i] = Math.min(dp[i-2] + cost[i-2], dp[i-1] + cost[i-1]);
+        }
+        return dp[len];
+    }
+
+}
