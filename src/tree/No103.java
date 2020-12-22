1de16ca0a7dbef6a333dade81589312ad3b92b20
rayyu999 <411205547@qq.com>
2020-12-22 21:46:02 +0800

    lc 103

--- src/tree/No103.java
+++ src/tree/No103.java
@@ -9,36 +9,34 @@ import java.util.Queue;
 
 public class No103 {
 
-    class Solution {
-        public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-            List<List<Integer>> res = new ArrayList<>();
-            if (root != null) {
-                Queue<TreeNode> queue = new LinkedList<>();
-                queue.offer(root);
-                TreeNode p;
-                // 第一层为从左往右遍历
-                Boolean leftToRight = true;
-                while (queue.size() > 0) {
-                    int cnt = queue.size();
-                    List<Integer> layer = new ArrayList<>();
-                    while (cnt > 0) {
-                        p = queue.poll();
-                        if (leftToRight) {
-                            layer.add(p.val);
-                        } else {
-                            layer.add(0, p.val);
-                        }
-                        if (p.left != null) queue.offer(p.left);
-                        if (p.right != null) queue.offer(p.right);
-                        --cnt;
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root != null) {
+            Queue<TreeNode> queue = new LinkedList<>();
+            queue.offer(root);
+            TreeNode p;
+            // 第一层为从左往右遍历
+            Boolean leftToRight = true;
+            while (queue.size() > 0) {
+                int cnt = queue.size();
+                List<Integer> layer = new ArrayList<>();
+                while (cnt > 0) {
+                    p = queue.poll();
+                    if (leftToRight) {
+                        layer.add(p.val);
+                    } else {
+                        layer.add(0, p.val);
                     }
-                    // 下一层的遍历顺序反转
-                    leftToRight = !leftToRight;
-                    res.add(layer);
+                    if (p.left != null) queue.offer(p.left);
+                    if (p.right != null) queue.offer(p.right);
+                    --cnt;
                 }
+                // 下一层的遍历顺序反转
+                leftToRight = !leftToRight;
+                res.add(layer);
             }
-            return res;
         }
+        return res;
     }
 
 }
