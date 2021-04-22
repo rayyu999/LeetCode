b124d227c5151a662c48505c246b40b205ac1b6a
rayyu999 <411205547@qq.com>
2021-04-22 22:36:16 +0800

    lc 516

--- src/test/Main.java
+++ src/test/Main.java
@@ -1,16 +1,15 @@
 package test;
 
-import dfs.No46;
+
+import dp.No516;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        No46 no46 = new No46();
+        No516 no516 = new No516();
 
-        int[] a = new int[]{1,2,3};
-        no46.permute(a);
-        System.out.println(a);
+        System.out.println(no516.longestPalindromeSubseq("abcda"));
 
     }
 
